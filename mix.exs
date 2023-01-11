8cbe55a83deb473832b9666da1bc9ee44f56854f
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-01-11 17:53:22 +0100

    Reflect changes in astarte_core lib
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Astarte.DataAccess.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, github: "astarte-platform/astarte_core", branch: "release-1.1"}
+      {:astarte_core, github: "annopaolo/astarte_core", branch: "policy_prefetch_count"}
     ]
   end
 
